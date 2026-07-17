b7aef09998775aa9e2f26ad780517a6ae45c9c36
jsgzwlwbq8 <LyallDorrell9532@hotmail.com>
2026-07-17 16:54:01 +0800

    修复云端登录组件加载

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "earth-collection-v20";
+const CACHE_NAME = "earth-collection-v21";
 const FILES = [
   "./",
   "./index.html",
   "./styles.css?v=12",
   "./china-cities.js?v=2",
   "./popular-cities.js?v=1",
-  "./app.js?v=14",
+  "./app.js?v=15",
   "./account-sync.js?v=1",
   "./local-share.js?v=1",
   "./shared-diary.js?v=2",
